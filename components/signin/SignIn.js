7f5977c9193c9488893e53aff0642725105ac1a3
Sajeel Ahmad <52258972+sajeell@users.noreply.github.com>
2021-05-27 22:23:57 +0500

    :construction: Prepare SignIn Screen

--- components/signin/SignIn.js
+++ components/signin/SignIn.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {
     SafeAreaView,
+    View,
+    Image,
     StyleSheet,
-    Text
 } from 'react-native';
 
+import bgImage from "../../static/images/form-bg.png"
 const SignIn = () => {
 
     return (
         <SafeAreaView style={styles.container}>
-            <Text>Hello</Text>
+            <View style={styles.imageContainer}>
+                <Image source={bgImage} style={styles.image}></Image>
+            </View>
         </SafeAreaView>
     );
 };
@@ -18,8 +22,17 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: "#d0ddfb",
         display: 'flex',
+        height: '100%',
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    imageContainer: {
+    },
+    image: {
+        position: 'relative',
+        zIndex: 1,
+        width: 365,
+        height: 315
     }
 });
 
