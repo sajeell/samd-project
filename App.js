8d10ce58315519e2559f2b1bb8b7d4b8c1f2bf9a
Sajeel Ahmad <52258972+sajeell@users.noreply.github.com>
2021-05-28 11:47:36 +0500

    :tada: Complete SignIn Page

--- App.js
+++ App.js
@@ -3,25 +3,18 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet
-} from 'react-native';
-import SignIn from './components/signin/SignIn.js';
-
+import React from 'react'
+import {SafeAreaView, StyleSheet} from 'react-native'
+import SignIn from './components/signin/SignIn.js'
 
 const App = () => {
-
   return (
     <SafeAreaView>
       <SignIn />
     </SafeAreaView>
-  );
-};
+  )
+}
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({})
 
-});
-
-export default App;
+export default App
--- components/signin/SignIn.js
+++ components/signin/SignIn.js
@@ -1,39 +1,106 @@
-import React from 'react';
+import React from 'react'
 import {
     SafeAreaView,
     View,
+    Text,
     Image,
     StyleSheet,
-} from 'react-native';
+    TextInput,
+    Pressable,
+    Button,
+} from 'react-native'
 
-import bgImage from "../../static/images/form-bg.png"
+import bgImage from '../../static/images/form-bg.png'
 const SignIn = () => {
-
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.imageContainer}>
                 <Image source={bgImage} style={styles.image}></Image>
             </View>
+            <View style={styles.formContainer}>
+                <Text style={styles.formRow1}>Sign In</Text>
+                <View style={styles.formRow2}>
+                    <TextInput
+                        placeholder="Email Address"
+                        keyboardType="email-address"
+                        style={styles.inputField}
+                    />
+                </View>
+                <View style={styles.formRow3}>
+                    <TextInput
+                        placeholder="Password"
+                        secureTextEntry={true}
+                        style={styles.inputField}
+                    />
+                </View>
+                <View >
+                    <Pressable style={styles.formRow4}>
+                        <Button title="Submit" color="#405e87" />
+                    </Pressable>
+                </View>
+                <View style={styles.formRow5}>
+                    <Text>Sign Up here</Text>
+                </View>
+            </View>
         </SafeAreaView>
-    );
-};
+    )
+}
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: "#d0ddfb",
+        backgroundColor: '#d0ddfb',
         display: 'flex',
         height: '100%',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
-    imageContainer: {
+    imageContainer: {},
+    formContainer: {
+        zIndex: 100,
+        position: 'absolute',
+        backgroundColor: '#f2f6ff',
+        borderRadius: 15,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: 60
     },
     image: {
         position: 'relative',
         zIndex: 1,
         width: 365,
-        height: 315
-    }
-});
+        height: 315,
+    },
+    formRow1: {
+        fontSize: 18,
+        marginBottom: 20
+    },
+    formRow2: {
+        marginBottom: 20,
+        backgroundColor: "white",
+        paddingHorizontal: 60,
+        borderRadius: 10
+    },
+    formRow3: {
+        marginBottom: 5,
+        backgroundColor: "white",
+        paddingHorizontal: 70,
+        borderRadius: 10
+    },
+    formRow4: {
+        marginBottom: 5,
+        marginTop: 16,
+        width: 150,
+        borderRadius: 10
+    },
+    formRow5: {
+        display: 'flex',
+        alignSelf: 'flex-start',
+        marginLeft: 8,
+        marginTop: 10,
+        fontSize: 12
+    },
 
-export default SignIn;
+})
+
+export default SignIn
